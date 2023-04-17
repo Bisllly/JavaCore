cf1b37470bdd2e30dd3bb8e0cfddc42b2a3f97ac
Bisllly <cuongnguyen3900@gmail.com>
2023-04-17 23:03:48 +0700

    adding comments on how I understand classes and objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+//JAVA CLASSES AND OBJECTS
+
+//every executable program must be in a class, where program execution begins
 public class Main {
+    //This line declares an integer variable called x and assigns the value 5 to it.
+    // Since this variable is not `static`, it belongs to every instance of the `Main` class,
+    // and can be accessed by any method within an instance of that class.
     int x = 5;
+
 
     public static void main(String[] args) {
         Main myObj = new Main();
